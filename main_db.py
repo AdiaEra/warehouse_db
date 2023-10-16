ad43c6daba8cd8d76f6a0be3711a5873a9b72814
Svetlana Shtylevskaya <adiaerena@gmail.com>
2023-10-16 13:57:25 +0300

    5

--- main_db.py
+++ main_db.py
@@ -15,7 +15,7 @@ def add_order(order_id):
         cur.fetchone()
         if cur.fetchone() is None:
             try:
-                insert_query = """INSERT INTO order_number(order_id) 
+                insert_query = """INSERT INTO order_number(order_id)
                                   VALUES (%s)"""
                 cur.execute(insert_query, (order_id,))
             except UniqueViolation:
@@ -23,7 +23,7 @@ def add_order(order_id):
         return 'Новый заказ добавлен'
 
 
-order = 'YQ-1'
+order = 'YY-88'
 # print(add_order(order))
 conn.commit()
 
@@ -68,16 +68,17 @@ def data_without_order(rack: str, shelf: int, category: str, quantity: int):
         return f'Информация записана'
 
 
-data_rack = 'А-1'
-data_shelf = 1
-data_category = 'свечи автомобильные'
-data_quantity = 430
+data_rack = 'C-2'
+data_shelf = 5
+data_category = 'автошины'
+data_quantity = 44
 
 # print(data_without_order(data_rack, data_shelf, data_category, data_quantity))
 conn.commit()
 
 
-def search_id(rack: str, shelf: int, category: str):
+def search_id(rack: str, shelf: int,
+              category: str):  # поиск id (функция ниже(data_order_number) - для записи номера заказа по найденному id)
     """
     Функция вывода id по заданным параметрам
     :param rack: стеллаж
@@ -136,20 +137,22 @@ order = '34Y-ui'
 conn.commit()
 
 
-def delete_order(rack: str, shelf: int, category: str):
+def delete_without_order_number(rack: str, shelf: int, category: str):
     """
     Функция удаления данных, по хранящимся материалам (без номера заказа)
     :return: Данные удалёны из БД
     """
     with conn.cursor() as cur:
-        delete_query = """DELETE FROM order_number WHERE rack = %s
+        delete_query = """DELETE FROM order_information WHERE rack = %s
         AND shelf = %s AND category = %s"""
         cur.execute(delete_query, (rack, shelf, category))
         return f'Данные удалёны из БД'
 
 
-order = '34Y-ui'
-# print(delete_order())
+data_rack = 'А-1'
+data_shelf = 1
+data_category = 'свечи автомобильные'
+# print(delete_without_order_number(data_rack, data_shelf, data_category))
 conn.commit()
 
 
@@ -168,7 +171,7 @@ def search_data(order_id):
 order = 'Yu-1'
 
 
-print(search_data(order))
+# print(search_data(order))
 
 def data_racks_shelf(rack: str, shelf: int):
     """
@@ -211,11 +214,11 @@ data_rack = 4
 def add_quantity(count: int, order_id: str, rack: str, shelf: int, category: str):
     """
     Функция добавления определённого количества материала в заказе
-    :param count:
-    :param order_id:
-    :param rack:
-    :param shelf:
-    :param category:
+    :param count: количество материала, которое добавляется
+    :param order_id: номер заказа
+    :param rack: номер стеллажа
+    :param shelf: номер полки
+    :param category: наименование материала
     :return:
     """
     with conn.cursor() as cur:
@@ -226,15 +229,15 @@ def add_quantity(count: int, order_id: str, rack: str, shelf: int, category: str
 
 
 number_order = 'Yu-1'
-order_category = 'переключатели'
-order_rack = 'E-1'
-order_shelf = 4
-order_count = 333
+order_category = 'каска'
+order_rack = 'E-2'
+order_shelf = 3
+order_count = 1333
 # print(add_quantity(order_count, number_order, order_rack, order_shelf, order_category))
 conn.commit()
 
 
-def subtract_the_amount(count: int, order_id: str, rack: int, shelf: int, category: str):
+def subtract_the_amount(count: int, order_id: str, rack: str, shelf: int, category: str):
     """
     Функция уменьшения определённого количества материала в заказе
     :param count: количество, на которое уменьшается материал
@@ -295,10 +298,9 @@ def data_from_shelf(rack: str, shelf: int):
 
 
 data_rack = 'E-2'
-data_shelf = 3
+data_shelf = 2
 
-
-# print(data_from_shelf(data_rack, data_shelf))
+print(data_from_shelf(data_rack, data_shelf))
 
 
 def search_rack_shelf(category):
@@ -327,7 +329,7 @@ data_category = 'каска'
 def search_rack_shelf_2(order_id):
     """
     Функция поиска номера стеллажа и полки, на которой находится определённый материал
-    :param category: наименование материала
+    :param order_id: наименование материала
     :return: номер стеллажа и полки
     """
     with conn.cursor() as cur:
@@ -337,7 +339,96 @@ def search_rack_shelf_2(order_id):
 
 
 order = 'Yu-1'
+
+
 # print(search_rack_shelf_2(order))
+
+
+def list_rack():
+    """
+    Функция для получения списка стеллажей
+    :return: список стеллажей
+    """
+    with conn.cursor() as cur:
+        select_query = """SELECT rack FROM order_information"""
+        cur.execute(select_query)
+        list_1 = []
+        for i in cur.fetchall():
+            for item in i:
+                list_1.append(item)
+        return list_1
+
+
+# print(list_rack())
+
+
+def list_shelf(rack: str):
+    """
+    Функция показывает полки на определённом стеллаже
+    :param rack: стеллаж
+    :return: список полок
+    """
+    with conn.cursor() as cur:
+        select_query = """SELECT shelf FROM order_information WHERE rack = %s"""
+        cur.execute(select_query, (rack,))
+        list_1 = []
+        for i in cur.fetchall():
+            for item in i:
+                list_1.append(item)
+        return list_1
+
+
+data_rack = 'E-2'
+
+
+# print(list_shelf(data_rack))
+
+def subtract_the_amount_without_order_number(count: int, rack: str, shelf: int, category: str):
+    """
+    Функция уменьшения определённого количества материала
+    :param count: количество, на которое уменьшается материал
+    :param rack: номер стеллажа
+    :param shelf: номер полки
+    :param category: наименование материала
+    :return: Количество материала "{category}" в заказе № {order_id}, хранящегося на {shelf} полке {rack} стеллажа, уменьшено на {count}
+    """
+    with conn.cursor() as cur:
+        select_query = """UPDATE order_information SET quantity = quantity - %s WHERE rack = %s
+        AND shelf = %s AND category = %s"""
+        cur.execute(select_query, (count, rack, shelf, category))
+        return f'Количество материала "{category}", хранящегося на {shelf} полке {rack} стеллажа, уменьшено на {count}'
+
+
+data_rack = 'C-2'
+data_shelf = 5
+data_category = 'автошины'
+order_count = 44
+# print(subtract_the_amount_without_order_number(order_count, data_rack, data_shelf, data_category))
+conn.commit()
+
+
+def add_quantity_without_order_number(count: int, rack: str, shelf: int, category: str):
+    """
+    Функция добавления определённого количества материала в заказе
+    :param count: количество материала, которое добавляется
+    :param rack: номер стеллажа
+    :param shelf: номер полки
+    :param category: наименование материала
+    :return:
+    """
+    with conn.cursor() as cur:
+        select_query = """UPDATE order_information SET quantity = quantity + %s WHERE rack = %s
+        AND shelf = %s AND category = %s"""
+        cur.execute(select_query, (count, rack, shelf, category))
+        return f'Количество материала "{category}", хранящегося на {shelf} полке {rack} стеллажа, увеличино на {count}'
+
+
+data_rack = 'C-2'
+data_shelf = 5
+data_category = 'автошины'
+order_count = 44
+# print(add_quantity_without_order_number(order_count, data_rack, data_shelf, data_category))
+conn.commit()
 
 # def search_data(order_id):
 #     with conn.cursor() as cur:
